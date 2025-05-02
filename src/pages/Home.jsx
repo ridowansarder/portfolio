65a9cb86f3c237390f0dbe07f723836cb0720e02
Ridwan Sarder <ridwanzuraiz@gmail.com>
2025-05-02 21:39:52 +0600

    second commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='min-h-screen'>
+        {/* Theme toggler */}
+
+        {/* Background effects */}
+
+        {/* Navbar */}
+
+        {/* Main content */}
+
+        {/* Footer */}
+    </div>
+    
+  )
+}
+
+export default Home
